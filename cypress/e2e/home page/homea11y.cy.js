edaa754f6248df49551433e340566b65f050a372
Robert Dowling <gitlab.aspiritech.org>
2024-06-14 14:34:55 -0500

    fixed code

--- cypress/e2e/home page/homea11y.cy.js
+++ cypress/e2e/home page/homea11y.cy.js
@@ -7,13 +7,13 @@ describe('template spec', () => {
     // Basic usage
 it('Has no detectable a11y violations on load', () => {
     // Test the page at initial load
-    cy.checkA11y().assertNoViolations()
+    cy.checkA11y()
   })
   
   // Applying a context and run parameters
   it('Has no detectable a11y violations on load (with custom parameters)', () => {
     // Test the page at initial load (with context and options)
-    cy.checkA11y('.example-class', {
+    cy.checkA11y('button', {
       runOnly: {
         type: 'tag',
         values: ['wcag2a']
@@ -24,7 +24,7 @@ it('Has no detectable a11y violations on load', () => {
   it('Has no detectable a11y violations on load (filtering to only include critical impact violations)', () => {
     // Test on initial load, only report and assert for critical impact items
     cy.checkA11y(null, {
-      includedImpacts: ['critical']
+      includedImpacts: ['critical', 'high', 'medium', 'low']
     })
   })
   
