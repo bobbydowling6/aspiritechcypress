4d565d573716fec8b25aa4c4268b97ef1c2af796
Robert Dowling <gitlab.aspiritech.org>
2024-06-20 09:18:11 -0500

    Updated Cypress to 13.12 and added more source code to Footer Model file and referenced those changes to Footer.cy.js file

--- cypress/support/PageObjects/footer.js
+++ cypress/support/PageObjects/footer.js
@@ -1,5 +1,48 @@
 class Footer
 {
-
+    annualReport2015()
+    {
+        return cy.contains('a', '2015').click()
+    }
+    annualReport2016()
+    {
+        return cy.contains('a', '2016').click()
+    }
+    annualReport2017()
+    {
+        return cy.contains('a', '2017').click()
+    }
+    annualReport2018()
+    {
+        return cy.contains('a', '2018').click()
+    }
+    annualReport2019()
+    {
+        return cy.contains('a', '2019').click()
+    }
+    annualReport2020()
+    {
+        return cy.contains('a', '2020').click()
+    }
+    annualReport2021()
+    {
+        return cy.contains('a', '2021').click()
+    }
+    annualReport2022()
+    {
+        return cy.get('[href="https://issuu.com/aspiritech/docs/annual_report_2022_2_"]').click()
+    }
+    siteMap()
+    {
+        return cy.get('.elementor-element-f5e176b > .elementor-widget-container > :nth-child(1) > a').contains('Sitemap').click()
+    }
+    privacy()
+    {
+        return cy.get('.elementor-widget-container > :nth-child(2) > a').contains('Privacy').click()
+    }
+    backToTop()
+    {
+        return cy.get('.elementor-widget-container > :nth-child(3) > a').contains('Back to top').click()
+    }
 }
 export default Footer
